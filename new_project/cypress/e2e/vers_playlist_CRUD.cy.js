680b1955b726aa9d98e531b112862c0d75f45137
Shaykh102 <dignity000@gmail.com>
2023-02-12 23:49:53 +0500

    new_spec_examples

--- new_project/cypress/e2e/vers_playlist_CRUD.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-// Method for filling out the login form
-fillForm(username, password, confirmPassword); {
-    this.usernameInput.type(username);
-    this.passwordInput.type(password);
-    this.confirmPasswordInput.type(confirmPassword);
-}
